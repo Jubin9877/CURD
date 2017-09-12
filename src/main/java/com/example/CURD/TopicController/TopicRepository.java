4b8c00042160ffadc1fabc49871968a2399d8510
Jubin9877 <pateljubin92@gmail.com>
2017-09-12 14:13:38 +0530

    Insert With Database

---
+++ src/main/java/com/example/CURD/TopicController/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.example.CURD.TopicController;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+	
+}
--- src/main/java/com/example/CURD/TopicController/TopicService.java
+++ src/main/java/com/example/CURD/TopicController/TopicService.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicrepository;
 
 	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring","Spring Framework","Spring Freamework Description"),
@@ -16,6 +20,10 @@ public class TopicService {
 			));
 	
 	public List<Topic> getAllTopics(){
+		//return topics;
+		List<Topic> topics = new ArrayList<>();
+		topicrepository.findAll()
+		.forEach(topics :: add);
 		return topics;
 	}
 	
@@ -25,7 +33,7 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-	topics.add(topic);
+		topicrepository.save(topic);
 		
 	}
 
